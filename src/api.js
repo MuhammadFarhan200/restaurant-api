4b696ca12c79ab0ba57a9ea81b1ee5ca6e541954
MuhammadFarhan200 <farhan_200@smkassalaambandung.sch.id>
2023-03-01 11:33:38 +0700

    commit

---
+++ src/api.js
@@ -0,0 +1,168 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+const router = express.Router();
+
+const restaurant = [
+  {
+    "id": 1,
+    "restaurant_name": "Burger King",
+    "restaurant_address": "1234 Main Street",
+    "restaurant_city": "New York",
+    "data": [
+      {
+        "foods": [
+          {
+            "id": 1,
+            "food_name": "Hors d'oeuvres",
+            "food_image": "https://asset.kompas.com/crops/QeIh8K69PV6aImLjg-71LU8i5ds=/63x35:943x622/750x500/data/photo/2021/12/09/61b17b708bfb3.jpg",
+            "food_price": "Rp. 150.000.00",
+          }, 
+          {
+            "id": 2,
+            "food_name": "Amuse-bouche",
+            "food_image": "https://asset.kompas.com/crops/_1ATzeSFjUNIopEuwToM5vq3poo=/50x60:850x594/750x500/data/photo/2021/12/09/61b17be84f720.jpg",  
+            "food_price": "Rp. 150.000.00",
+          },
+          {
+            "id": 3,
+            "food_name": "Sup",
+            "food_image": "https://asset.kompas.com/crops/94nQD1EAzV844CTyQtBHII2JAJA=/0x68:1179x854/750x500/data/photo/2021/04/26/60860684bff18.jpg",  
+            "food_price": "Rp. 150.000.00",
+          },
+          {
+            "id": 4,
+            "food_name": "Appetizer",
+            "food_image": "https://asset.kompas.com/crops/lIjufSaA8AOQiSxLvsCr9T4FjCQ=/32x6:1000x652/750x500/data/photo/2021/12/09/61b17c94a0810.jpg",  
+            "food_price": "Rp. 150.000.00",
+          },
+          {
+            "id": 5,
+            "food_name": "Salad",
+            "food_image": "https://asset.kompas.com/crops/qTXcyWnCBVUNHJnX6MooX4E5tgU=/114x0:1701x1058/750x500/data/photo/2021/02/09/60222ba484230.jpeg",  
+            "food_price": "Rp. 150.000.00",
+          },
+          {
+            "id": 6,
+            "food_name": "Ikan",
+            "food_image": "https://asset.kompas.com/crops/fl7dGjvTEXjia5weFSYa_y7rRAk=/0x0:1000x667/750x500/data/photo/2021/06/21/60d032db027c1.jpg",  
+            "food_price": "Rp. 150.000.00",
+          },
+          {
+            "id": 7,
+            "food_name": "First main course",
+            "food_image": "https://asset.kompas.com/crops/gUV1fMVRPgczTocdI6rcPDTnJFc=/100x67:900x600/750x500/data/photo/2021/07/17/60f2b8d0c1a89.jpg",  
+            "food_price": "Rp. 150.000.00",
+          },
+          {
+            "id": 8,
+            "food_name": "Palate cleanser",
+            "food_image": "https://asset.kompas.com/crops/qrx9lTr6OSi1qJ_OaJBGGqi7J7g=/0x0:1000x667/750x500/data/photo/2021/07/17/60f2a8cfb2c9b.jpg",  
+            "food_price": "Rp. 150.000.00",
+          },
+          {
+            "id": 9,
+            "food_name": "Second main course",
+            "food_image": "https://asset.kompas.com/crops/GzHYx9YsIAfNcJlFDzd60Zwu0hM=/0x0:1000x667/750x500/data/photo/2021/07/18/60f40dc4f1b44.jpg",  
+            "food_price": "Rp. 150.000.00",
+          },
+          {
+            "id": 10,
+            "food_name": "Dessert",
+            "food_image": "https://asset.kompas.com/crops/oM6wJ2Uf7tphBeIReWx9EY7T96U=/26x36:1357x923/750x500/data/photo/2021/01/05/5ff498f8d34af.jpg",  
+            "food_price": "Rp. 150.000.00",
+          },
+          {
+            "id": 11,
+            "food_name": "Mignardise",
+            "food_image": "https://asset.kompas.com/crops/eM1CWvjJQ8adcTIvHGcLKwXB87M=/0x0:1000x667/750x500/data/photo/2021/10/27/6178b4b7cf154.jpeg",  
+            "food_price": "Rp. 150.000.00",
+          },
+        ],
+        "drinks": [
+          {
+            "id": 1,
+            "drink_name": "Ice Blend",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/ice-blend-coffee-bean.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 2,
+            "drink_name": "Ice Drink",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/ice-drink-4.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 3,
+            "drink_name": "Milk Shake",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/milkshake-4.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 4,
+            "drink_name": "Fruit Tea",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/fruit-tea-2.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 5,
+            "drink_name": "Flavoured Tea",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/hot-tea1.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 6,
+            "drink_name": "Flavoured Soda",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/511627718.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 7,
+            "drink_name": "Latte",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/511627718.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 8,
+            "drink_name": "Macchiato",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/starbucks-iced-hazelnut-macchiato.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 9,
+            "drink_name": "Mocktail",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/mocktails2.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 10,
+            "drink_name": "Cocktail",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/central-bar-cocktails.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+          {
+            "id": 11,
+            "drink_name": "Mojito",
+            "drink_image": "https://i0.wp.com/indobubbletea.blog/wp-content/uploads/2014/11/smirnoff-apple-mojito-white-stores-alternative-cocktail-recipe-drink.jpg",
+            "drink_price": "Rp. 40.000.00",
+          },
+        ],
+      }
+    ]
+  }
+];
+
+router.get('/restaurant', (req, res) => {
+  res.send(restaurant);
+});
+
+app.listen(3000, () => {
+  console.log('Server berjalan pada port 3000');
+  console.log('Open Result: http://localhost:3000/.netlify/functions/api/');
+});
+
+app.use('/.netlify/functions/api', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
